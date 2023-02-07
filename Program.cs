d9fcb166f5d753bc622fdc1173036b1a6f53cbda
Vadim <Po4ta.com>
2023-02-07 11:29:34 +0300

    3

--- Program.cs
+++ Program.cs
@@ -54,18 +54,103 @@
 // 7 -> да
 // 1 -> нет
 
- Console.Write(" Нужно узнать, является ли этот день выходным или нет?  "); 
- while (true) 
- { 
- Console.Write("\n Введите номер дня недели  "); 
- int num = int.Parse(Console.ReadLine()); 
- if (num >= 1 && num <= 5) 
-  { 
- Console.Write(" Нет, это не выходной"); 
-} 
-else if (num >= 6 && num <= 7) 
- { 
- Console.Write(" Да, это выходной"); 
- } 
-else Console.Write(" Не существует такого дня недели"); 
- } 
+//  Console.Write(" Нужно узнать, является ли этот день выходным или нет?  "); 
+//  while (true) 
+//  { 
+//  Console.Write("\n Введите номер дня недели  "); 
+//  int num = int.Parse(Console.ReadLine()); 
+//  if (num >= 1 && num <= 5) 
+//   { 
+//  Console.Write(" Нет, это не выходной"); 
+// } 
+// else if (num >= 6 && num <= 7) 
+//  { 
+//  Console.Write(" Да, это выходной"); 
+//  } 
+// else Console.Write(" Не существует такого дня недели"); 
+//  } 
+
+
+
+
+
+
+// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// void CheckPalindromicNumber(int number)
+// {
+// if(number >= 10000)
+//  {
+// int division1 = number / 10000;
+//  int remainder1 = number % 10;
+// if(division1 == remainder1)
+// {
+// number = number / 10;
+// int division2 = (number / 100) % 10;
+// int remainder2 = number % 10;
+// if(division2 == remainder2)
+// Console.WriteLine("Да");
+// }
+//  else 
+//  Console.WriteLine("Нет");
+            
+//  }
+// else
+//  Console.WriteLine("Некорректное число!");
+// }
+ 
+// Console.WriteLine("Введите пятизначное число:");
+// int number = int.Parse(Console.ReadLine()!);
+// CheckPalindromicNumber(number);
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// using System;
+// namespace New_Project
+// { class Program
+// { public static void Main()
+// { double [] x = new double [6];
+// double d, distance = 0.0;
+// Console.WriteLine("x1 y1 z1 x2 y2 z3: Числа записывать через пробел");
+// var line = Console.ReadLine().Split();
+// for (int i = 0; i < 3; i++)
+// { d = Convert.ToDouble(line[i + 3])
+// - Convert.ToDouble(line[i]);
+// distance += d * d; }
+// Console.WriteLine("r = " + Math.Sqrt(distance)); } } }
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

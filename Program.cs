f567a9db500d84718ecf2dd097c622c9d7717a9c
Vadim <Po4ta.com>
2023-02-18 18:28:06 +0300

    7

--- Program.cs
+++ Program.cs
@@ -85,23 +85,41 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
-int[] arr = Console.ReadLine()
-                .Split(' ', ',', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-int lenArray = ReadInt("Введите длинну массива: ");
+// int[] arr = Console.ReadLine()
+//                 .Split(' ', ',', StringSplitOptions.RemoveEmptyEntries)
+//                 .Select(int.Parse)
+//                 .ToArray();
+// int lenArray = ReadInt("Введите длинну массива: ");
 
-int[] randomArray = new int[lenArray];
-for (int i = 0; i < randomArray.Length; i++)
+// int[] randomArray = new int[lenArray];
+// for (int i = 0; i < randomArray.Length; i++)
+// {
+//     randomArray[i] = new Random().Next(1, 9);
+//     Console.Write(randomArray[i] + " ");
+// }
+
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+Console.Write("Введите значение N:");
+int elementsCount=int.Parse(Console.ReadLine());
+int[] myArray=new int[elementsCount];
+
+for(int i=0; i<myArray.Length; i++)
 {
-    randomArray[i] = new Random().Next(1, 9);
-    Console.Write(randomArray[i] + " ");
+Console.Write($"Введите элемент массива под индексом {i}:\t");
+myArray[i]=int.Parse(Console.ReadLine());
 }
+Console.WriteLine("Вывод массива");
 
-
-// Функция ввода
-int ReadInt(string message)
+for (int i=0;i<myArray.Length; i++)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("["+(myArray[i]+"]");
 }
+Console.ReadLine();

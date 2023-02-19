dc0b15f780891c1c7928a326897756602e68e43f
Vadim <Po4ta.com>
2023-02-19 08:44:05 +0300

    9

--- Program.cs
+++ Program.cs
@@ -1,102 +1,11 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// 3, 5 -> 243 (3⁵)
-
-// 2, 4 -> 16
-
-
-
-// int numberA = ReadInt("Введите число A: ");
-// int numberB = ReadInt("Введите число B: ");
-// ToDegree(numberA, numberB);
-
-
-// // Функция возведения в степень
-// void ToDegree(int a, int b)
-// {
-//     int result = 1;
-//     for (int i = 1; i <= b; i++)
-//     {
-//         result = result * a;
-//     }
-//     Console.WriteLine(result);
-// }
-
-// // Функция ввода
-// int ReadInt(string message)
-// {
-//     Console.WriteLine(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+// [345, 897, 568, 234] -> 2
 
 
 
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-// 452 -> 11
-
-// 82 -> 10
-
-// 9012 -> 12
-
-// int number = ReadInt("Введите число: ");
-// int len = NumberLen(number);
-// SumNumbers(number, len);
-
-
-// // Функция ввода
-// int ReadInt(string message)
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
-
-// // Функция подсчета цифр в числе
-// int NumberLen(int a)
-// {
-//     int index = 0;
-//     while (a > 0)
-//     {
-//         a /= 10;
-//         index++;
-//     }
-//     return index;
-// }
-
-// // Функция вывода суммы цифр в числе
-// void SumNumbers(int n, int len)
-// {
-//     int sum = 0;
-//     for (int i = 1; i <= len; i++)
-//     {
-//         sum += n % 10;
-//         n /= 10;
-//     }
-//     Console.WriteLine(sum);
-// }
 
 
 
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
-// 6, 1, 33 -> [6, 1, 33]
-
-Console.Write("Введите значение N:");
-int elementsCount = int.Parse(Console.ReadLine());
-int[] myArray = new int[elementsCount];
-
-for (int i = 0; i < myArray.Length; i++)
-{
-    Console.Write($"Введите элемент массива под индексом {i}:\t");
-    myArray[i] = int.Parse(Console.ReadLine());
-}
-Console.WriteLine("Вывод массива:");
-
-for (int i = 0; i < myArray.Length; i++)
-{
-    Console.WriteLine("[" + string.Join(",", myArray[i]) + "]");
-}
-Console.ReadLine();

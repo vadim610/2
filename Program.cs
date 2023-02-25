5b354b0ac6e285fc93cdafe225a0f113eb752348
Vadim <Po4ta.com>
2023-02-25 09:17:35 +0300

    12

--- Program.cs
+++ Program.cs
@@ -4,3 +4,43 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+

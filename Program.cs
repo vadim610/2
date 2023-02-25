7bbbed1882e79e4d54b9ee1d240a5ddef277f8a8
Vadim <Po4ta.com>
2023-02-25 09:22:03 +0300

    13

--- Program.cs
+++ Program.cs
@@ -1,46 +1,9 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-// 0, 7, 8, -2, -2 -> 2
+// m = 3, n = 4.
 
-// 1, -7, 567, 89, 223-> 3
+// 0,5 7 -2 -0,2
 
-// Console.Write("Введите элементы(через пробел): ");
-// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-// int count = 0;
+// 1 -3,3 8 -9,9
 
-// for (int i = 0; i < arr.Length; i++)
-// {
-//     if (arr[i] > 0)
-//     {
-//         count++;
-//     }
-// }
-
-// Console.WriteLine($"Кол-во элементов > 0: {count}");
-
-
-
-
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
-
-
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
-
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
-
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
-
+// 8 7,8 -7,1 9
